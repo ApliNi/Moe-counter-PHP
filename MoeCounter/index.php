398f82af4d11320094281ed3e4a5191a859df6bd
From-pErfo <59365724+From-pErfo@users.noreply.github.com>
2022-08-18 18:36:16 +0800

    修复CDN无法连接

--- MoeCounter/index.php
+++ MoeCounter/index.php
@@ -13,7 +13,7 @@ $c = array(
 
 
 	// 存放图片的目录, 普通路径结尾需要添加斜杠
-	'imgPath' => 'https://cdn.jsdelivr.net/gh/From-pErfo/Moe-counter-PHP@tree/main/MoeCounter/img/',
+	'imgPath' => 'https://fastly.jsdelivr.net/gh/From-pErfo/Moe-counter-PHP@tree/main/MoeCounter/img/',
 	// 图片名称前缀, xxx{0-9}.png
 	'imgNamePrefix' => 'gelbooru',
 	// 图片格式
